4b71f516d4c8bf116c34e3e64391fbe2285d00a0
marcserranog <marcserranog@gmail.com>
2024-11-17 02:02:00 +0100

    changing the web margin and the size of the cards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import WishlistPage from './pages/WishlistPage';
 import MovieDetailPage from './pages/MovieDetailPage';
-import WishlistPage from './pages/WishListPage';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movie/:id" element={<MovieDetailPage />} />
-        <Route path="/wishlist" element={<WishlistPage />} />
-      </Routes>
+      <div className="global-container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/wishlist" element={<WishlistPage />} />
+          <Route path="/movie/:id" element={<MovieDetailPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
