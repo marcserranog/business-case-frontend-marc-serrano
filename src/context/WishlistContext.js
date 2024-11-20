04a68857e33d312f09fdca7bf08d2244caaa28c5
marcserranog <marcserranog@gmail.com>
2024-11-20 01:24:38 +0100

    Optimizing Wishlist Context

--- src/context/WishlistContext.js
+++ src/context/WishlistContext.js
@@ -1,20 +1,25 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const WishlistContext = createContext();
 
 export const useWishlist = () => useContext(WishlistContext);
 
 export const WishlistProvider = ({ children }) => {
-  const [wishlist, setWishlist] = useState([]);
+  const [wishlist, setWishlist] = useState(() => {
+    const savedWishlist = localStorage.getItem('wishlist');
+    return savedWishlist ? JSON.parse(savedWishlist) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('wishlist', JSON.stringify(wishlist));
+  }, [wishlist]);
 
   const addToWishlist = (movie) => {
-    const isDuplicate = wishlist.some((item) => item.id === movie.id);
-    if (isDuplicate) {
-      return false;
-    } else {
-      setWishlist((prevWishlist) => [...prevWishlist, movie]);
-      return true;
+    if (wishlist.some((item) => item.id === movie.id)) {
+      return false; 
     }
+    setWishlist((prevWishlist) => [...prevWishlist, movie]);
+    return true;
   };
 
   const removeFromWishlist = (id) => {
