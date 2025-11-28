c4546d1a45336d7d592861a4038576056ef89131
marcserranog <marcserranog@gmail.com>
2025-11-28 01:08:54 +0100

    Fix publicPath for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/business-case-frontend-marc-serrano/',
   },
   module: {
     rules: [
